fd3d838bc0033b2814ea567ae84ca25615117132
hechenghan <hechenghan@bytedance.com>
2021-06-24 20:11:29 +0800

    # 编写图片生成器

--- matrix-python-project/cover_generator/main.py
+++ matrix-python-project/cover_generator/main.py
@@ -19,8 +19,8 @@ class Main(object):
         self.test_secord_title = "测试副标题测试副标题测"
         # self.db_handle = InstantDB().get_conncet()
 
-    def run(self):
-        Style().run()
+    def run_test(self):
+        Style(None).run()
 
         image_ext = ['.jpg', '.png', '.jpeg', '.bmp']
         for root, dirs, files in os.walk(self.image_path):
@@ -35,6 +35,27 @@ class Main(object):
                     os.remove("render/" + file)
 
 
+    @staticmethod
+    def run(record_id, first_title, secord_title):
+
+        Style(record_id).run()
+        image_list = []
+        image_ext = ['.jpg', '.png', '.jpeg', '.bmp']
+        for root, dirs, files in os.walk(record_id):
+            for file in files:
+                if os.path.splitext(file)[-1] in image_ext:
+                    image = Image.open(record_id + "_temp/" + file)
+                    Structure().run(first_title, secord_title)
+                    title = Image.open("transparent_title.png")
+                    r, g, b, a = title.split()
+                    image.paste(title, (0, 0), mask=a)
+                    new_image_name = record_id + "_fin/" + str(SnowId(1, 2, 0).get_id())[1:] + '.png'
+                    image.save(new_image_name, quality=100)
+                    image_list.append(new_image_name)
+
+        return image_list
+
+
 if __name__ == "__main__":
     m = Main()
-    m.run()
+    m.run_test()
