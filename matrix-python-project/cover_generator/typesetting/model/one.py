006a20832548e25485ff3e3a485960ca97c9d1e6
hechenghan <hechenghan@bytedance.com>
2021-06-25 16:53:53 +0800

    # 编写图片生成器

--- matrix-python-project/cover_generator/typesetting/model/one.py
+++ matrix-python-project/cover_generator/typesetting/model/one.py
@@ -35,7 +35,7 @@ class One(object):
         return rank_temp
 
     def build(self, image_list, model):
-        self.tb = Image.open("background.jpg")
+        self.tb = Image.open("cover_generator/background.jpg")
         self.image_list = image_list
         self.rank_model = model
         self.func_map[int(model["model_id"][1])]()
